e0bfafd5b5c7636c2b8473886ab5256909415f7d
abeprogoog-web <abeprogoog@gmail.com>
2026-09-14 21:21:10 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,26 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Abraham-Architect-Portfolio/',
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
+
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
+
   optimizeDeps: {
     esbuildOptions: {
       loader: {
